d2ab926e314d9adec96657952ba87247bf42f51d
Ethan Miller <ethan.romans5.8@gmail.com>
2019-06-08 18:04:13 -0500

    resize articles page with sidebar (change .container to .page-wrapper)

--- app.js
+++ app.js
@@ -113,6 +113,7 @@ app.use('/users', users);
 app.use('/messages', sermons);
 
 // Start Server
-app.listen(3000, function(){
-  console.log('Server started on port 3000...');
+let port = 3000;
+app.listen(port, function(){
+  console.log(`Server started on port ${port}...`);
 });
