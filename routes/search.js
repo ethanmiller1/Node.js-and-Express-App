ba04c601efab9f948ee75923c2412a77f5825e8e
Ethan Miller <ethan.romans5.8@gmail.com>
2019-07-06 12:35:05 -0500

    search articles

--- routes/search.js
+++ routes/search.js
@@ -7,17 +7,40 @@ let Article = require('../models/article');
 // Bring in User Model
 let User = require('../models/user');
 
-// Add Route
-router.get('/search', function(req, res){
-  Article.find({}, function(err, articles){
-    if(err){
-      console.log(err);
-    } else {
-      res.render('search', {
+// Search Post Route
+router.post('/search', function(req, res){
+  let search = req.body.search;
+
+  Article.find(
+    {$or:[
+      {title: {$regex : `.*${search}.*`}},
+      {abstract: {$regex : `.*${search}.*`}},
+      {author: {$regex : `.*${search}.*`}}
+    ]}, function(err, articles) {
+      if(err) {
+         console.log(err);
+      }
+      else{
+        res.render('search', {
         title:'Articles',
         articles: articles
       });
-    }
+      }
+  });
+});
+
+// Render search page.
+router.get('/search', function(req, res){
+  Article.find({}, function(err, articles) {
+      if(err) {
+         console.log(err);
+      }
+      else{
+        res.render('search', {
+        title:'Articles',
+        articles: articles
+      });
+      }
   });
 });
 
